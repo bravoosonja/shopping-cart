80c20336a095b3fcc8549babfb0faa14b1e8c560
bravoosonja <100341686+bravoosonja@users.noreply.github.com>
2022-06-14 17:00:27 +1000

    start Cart.js

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -6,5 +6,20 @@ import CartContext from '../../store/CartContext';
 import { Wrapper, Content, Text } from "./Cart.styles";
 
 const Cart = (props) => {
-    
+    const cartCtx = useContext(CartContext);
+
+    const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+    const hasItems = cartCtx.items.length > 0;
+
+    const cartItemRemoveHandler = (id) => {
+        cartCtx.removeItem(id);
+    };
+
+    const cartItemAddHandler = (item) => {
+        cartCtx.addItem({...item, amount:1});
+    };
+
+    const cartItems = (
+        
+    )
 }
